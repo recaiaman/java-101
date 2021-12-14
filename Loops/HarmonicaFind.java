205d7cb830b5d840d1745d7073cb8933a02fc816
blackeagle08 <recaiataman@outlook.com>
2021-12-14 11:51:26 +0300

    Practice 7 commited

---
+++ Loops/HarmonicaFind.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HarmonicaFind {
+    Scanner reader = new Scanner(System.in);
+    
+    System.out.print("N  sayisini giriniz...");
+    int numb = reader.nextInt();
+    double result = 0.0;
+
+    for(double i = 1; i<=n; i++){
+        result += (1/i);
+    }
+    System.out.print(result);
+}
